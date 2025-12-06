f3e42ba29c6cd9f0afcd088cab6bd2850de7f8ad
Lloyd Dakin <taidakin99lol@gmail.com>
2025-12-06 15:10:50 -0800

    Add Ant Design icons and implement login form in LoginPage component

--- apps/frontend/src/App.jsx
+++ apps/frontend/src/App.jsx
@@ -1,16 +1,25 @@
 import { useState } from 'react'
 import './App.css'
 import { Routes, Route } from 'react-router'
+import { ConfigProvider } from 'antd'
 import LoginPage from './Pages/login/login-page'
 import TodoPage from './Pages/to-do-list/todo-list-page'
 
 function App() {
   return (
     <>
-      <Routes>
-        <Route path='/' element={<LoginPage />} />
-        <Route path='/todos' element={<TodoPage />} />
-      </Routes>
+      <ConfigProvider
+        theme={{
+          token: {
+            colorPrimary: '#efb100',
+          },
+        }}
+      >
+        <Routes>
+          <Route path='/' element={<LoginPage />} />
+          <Route path='/todos' element={<TodoPage />} />
+        </Routes>
+      </ConfigProvider>
     </>
   )
 }
--- apps/frontend/src/Pages/login/login-page.tsx
+++ apps/frontend/src/Pages/login/login-page.tsx
@@ -1,4 +1,37 @@
+import { KeyOutlined, UserOutlined } from "@ant-design/icons";
+import { Button, Card, Form, Input, InputRef } from "antd";
+import { useEffect, useRef, useState } from "react";
 
 export default function LoginPage() {
-  return <div>Login Page</div>;
+  const [form] = Form.useForm();
+
+  const attemptLogin = (values: {username: string, password: string}) => {
+    // TODO create Zod schema for logins
+    // TODO use that login schema to validate username and password
+    console.log("Attempting login with", values.username, values.password);
+  };
+
+  return (
+    <div>
+      <Card className="w-64" title="Team-Do" variant="borderless">
+        <Form form={form} onFinish={attemptLogin}>
+          <Form.Item 
+            name="username" 
+            rules={[{required: true, message: "Please enter your username"}]}
+          >
+            <Input size="large" placeholder="Username" prefix={<UserOutlined />} />
+          </Form.Item>
+          <Form.Item 
+            name="password" 
+            rules={[{required: true, message: "Please enter your username"}]}
+          >
+            <Input size="large" placeholder="Password" prefix={<KeyOutlined />} type="password" />
+          </Form.Item>
+          <Form.Item>
+            <Button type="primary" block size="large" htmlType="submit" value={"Login"}>Login</Button>
+          </Form.Item>
+        </Form>
+      </Card>
+    </div>
+  );
 }
