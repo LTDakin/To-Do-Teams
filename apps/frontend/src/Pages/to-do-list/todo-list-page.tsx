bc6cb4c9a733bee3e5afa345d4ca35aa295ca4e7
Lloyd Dakin <taidakin99lol@gmail.com>
2025-12-24 15:45:41 -0800

    remove react css styles

--- apps/frontend/src/Pages/to-do-list/todo-list-page.tsx
+++ apps/frontend/src/Pages/to-do-list/todo-list-page.tsx
@@ -1,7 +1,10 @@
+import { Dropdown, MenuProps, Space } from "antd";
 import TodoItem from "./components/todo-item";
-import type { TodoDto } from "@team-do/types";
+import { SmileOutlined } from "@ant-design/icons";
+import { userAtom } from "../../state/user";
+import { useAtomValue } from "jotai";
 
-const samepleTodo: TodoDto = {
+const samepleTodo = {
   id: "1",
   title: "Sample Todo",
   completed: false,
@@ -11,9 +14,31 @@ const samepleTodo: TodoDto = {
   updatedAt: new Date(),
 };
 
+const items: MenuProps["items"] = [
+  {
+    key: "1",
+    label: "Logout",
+  },
+  {
+    key: "2",
+    danger: true,
+    label: "Delete Account",
+  },
+];
+
 export default function TodoListPage() {
+  const user = useAtomValue(userAtom);
+
   return (
-    <div>
+    <div className="min-h-screen min-w-screen">
+      <div className="flex justify-end items-center p-4">
+        <Dropdown menu={{ items }}>
+          <Space>
+            <SmileOutlined />
+            {user.username}
+          </Space>
+        </Dropdown>
+      </div>
       <h1>To-Do List</h1>
       <TodoItem todo={samepleTodo} />
     </div>
