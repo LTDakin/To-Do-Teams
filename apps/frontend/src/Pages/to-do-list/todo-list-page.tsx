49980ba23b53766677c3d45d895a9e70be94cb1c
Lloyd Dakin <taidakin99lol@gmail.com>
2026-01-06 14:31:03 -0800

    replace div with fragment

--- apps/frontend/src/Pages/to-do-list/todo-list-page.tsx
+++ apps/frontend/src/Pages/to-do-list/todo-list-page.tsx
@@ -3,13 +3,13 @@ import TodoList from "./components/todo-list";
 
 export default function TodoListPage() {
   return (
-    <div>
+    <>
       <div className="flex justify-end items-center p-4">
         <UserMenu />
       </div>
       <div className="todos-container flex flex-col items-center gap-4">
         <TodoList />
       </div>
-    </div>
+    </>
   );
 }
