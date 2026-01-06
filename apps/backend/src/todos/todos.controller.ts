5a8a731f286d953406d548221dc275f5ee4e9fd4
Lloyd Dakin <taidakin99lol@gmail.com>
2026-01-05 17:53:56 -0800

    instantiate todos resource using nest cli

---
+++ apps/backend/src/todos/todos.controller.ts
@@ -0,0 +1,40 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
+import { TodosService } from './todos.service';
+
+@Controller('todos')
+export class TodosController {
+  constructor(private readonly todosService: TodosService) {}
+
+  @Post()
+  create(@Body() createTodoDto) {
+    return this.todosService.create(createTodoDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.todosService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.todosService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateTodoDto) {
+    return this.todosService.update(+id, updateTodoDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.todosService.remove(+id);
+  }
+}
---
+++ apps/backend/src/todos/todos.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class TodosService {
+  create(createTodoDto: any) {
+    console.log(createTodoDto);
+    return 'This action adds a new todo';
+  }
+
+  findAll() {
+    return `This action returns all todos`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} todo`;
+  }
+
+  update(id: number, updateTodoDto: any) {
+    console.log(updateTodoDto);
+    return `This action updates a #${id} todo`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} todo`;
+  }
+}
